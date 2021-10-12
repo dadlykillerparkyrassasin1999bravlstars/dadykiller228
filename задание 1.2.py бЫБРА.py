87629266ffa8809e5aa29b42582b3a9085491e4d
dadlykillerparkyrassasin1999bravlstars <90253102+dadlykillerparkyrassasin1999bravlstars@users.noreply.github.com>
2021-10-12 17:47:43 +0700

    БЫБРА

---
+++ задание 1.2.py бЫБРА.py
@@ -0,0 +1,7 @@
+number = input('Введите 1 число - ')
+number2 = input('Введите 2 число - ')
+number3 = number
+number = number2
+number2 = number3
+print("Первое число" + "=" + " " + number)
+print("Второе число" + "=" + " " + number2)

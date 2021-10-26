183f9f67c13852bd3437edc52eb38f8071826507
BROK <90253102+dadlykillerparkyrassasin1999bravlstars@users.noreply.github.com>
2021-10-26 17:11:01 +0700

    СКА М бжбра хахаахахахахх АРРРР БИМЖ

---
+++ бжбра ахахахахахскам 4 или 3 зад.py
@@ -0,0 +1,7 @@
+import pyautogui
+import time
+name = input("Введи Имя")
+name1 = input("Введи Фамилию")
+name2 = input("Введи Отчество")
+pyautogui.write('10... 9... 8... 7... 6... 5... 4... 3... 2... 1...',interval=0.2)
+pyautogui.alert(text=name + name1 + name2 , title='СКАМ МАЛОЛЕТНИХ ВЫКИДЫШЕЙ', button='ААХАХАХХАХАХАХАХАХ РЕАЛ ЗАСКАМИЛ МАМОНТА ЗАШЕЛ НА АВИТО И ЗАСКАМИЛ ТАМ МАЛОЛЕТНЕГО МАМОНТА УУУУУУУ') 

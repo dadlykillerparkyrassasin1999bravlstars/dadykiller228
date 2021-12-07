8b4c4a033ff5c344c181cbc8e243dd2aafbb9800
BROK <90253102+dadlykillerparkyrassasin1999bravlstars@users.noreply.github.com>
2021-12-07 16:44:44 +0700

    аршргашщв

---
+++ 5 задание 04.py
@@ -0,0 +1,9 @@
+number = int(input())
+if number % 3 == 0:
+    print("Это число делится на 3")
+else:
+    print("Это число  не делится на 3")
+if number > 0:
+    print("Число больше 0")
+else:
+    print("Число меньше 0")

ada5b392185005d80e91cd8e8d69f0b02fece652
BROK <90253102+dadlykillerparkyrassasin1999bravlstars@users.noreply.github.com>
2021-12-21 16:38:48 +0700

    Add files via upload

---
+++ 4 задание цикл while.py
@@ -0,0 +1,13 @@
+import pyautogui
+import keyboard
+import time
+time.sleep(4)
+while True:
+    pyautogui.click(button = 'right')
+    time.sleep(0.25)
+    pyautogui.click(button = 'right')
+    time.sleep(0.25)
+    pyautogui.click(button = 'right')
+    time.sleep(0.25)
+    pyautogui.click(button = 'right')
+    time.sleep(0.25)

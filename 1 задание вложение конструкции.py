510be9dbacdcc0cf1697256144c16436d4628368
BROK <90253102+dadlykillerparkyrassasin1999bravlstars@users.noreply.github.com>
2022-02-01 17:55:30 +0700

    Add files via upload

---
+++ 1 задание вложение конструкции.py
@@ -0,0 +1,5 @@
+comanda = input("Введите какуюто команду start , coma, kal")
+if comanda == "start":
+    print("Эта омандавременно не оступна")
+elif comanda == "coma":
+    print("Если вам чтото итересноивы чем-то интерисутесь напишие YES если нет то NO "):

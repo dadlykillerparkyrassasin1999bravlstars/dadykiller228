d0ce9c756bb87e67b1a3e9d284d6a6f553b6a40d
BROK <90253102+dadlykillerparkyrassasin1999bravlstars@users.noreply.github.com>
2022-02-22 16:52:10 +0700

    Add files via upload

---
+++ 1 задание списски.py
@@ -0,0 +1,7 @@
+prok = input("Введите продукт котрый вы хотите внести в список           ")
+proki = input("Введите продукт котрый вы хотите внести в список           ")
+products = ["хлеб","масло","банан","мидоры","огурцы"]
+products.append(prok)
+products.append(proki)
+for prod in products:
+    print(prod)
